4dea30eec10385fd47a5c6a4e21b371dde11d0d2
Marcos Henrique <marcosmarcone48@gmail.com>
2022-01-30 00:22:51 -0300

    Suporte ao encaminhamento para o Whats.
    Ao clicar no botão "Fechar Pedido", o usuário é encaminhado para o WhatsApp com as informações do pedido.

--- scripts/script.js
+++ scripts/script.js
@@ -87,9 +87,11 @@ function selecionarSobremesa(classeSobremesa, tituloSobremesa, precoSobremesa) {
 /*Habilita o botão para fechar o pedido,
   caso as três opções estejam selecionadas.*/
 function habilitarBotaoFecharPedido() {
-    if ((pratoSelecionado !== null) && 
-    (bebidaSelecionada !== null) && 
-    (sobremesaSelecionada !== null)) {
+    // Verifica se há três opções selecionadas
+    if ((pratoSelecionado !== null) &&
+        (bebidaSelecionada !== null) &&
+        (sobremesaSelecionada !== null)) {
+        // Habilita o botão para fechar o pedido
         const botaoFecharPedido = document.querySelector(".fechar-pedido__botao");
         botaoFecharPedido.disabled = false;
         botaoFecharPedido.classList.add("tres_opcoes_selecionados");
@@ -97,6 +99,22 @@ function habilitarBotaoFecharPedido() {
     }
 }
 
+/* Implementa o funcionamento do botão "Fechar Pedido */
 function fecharPedido() {
-    alert("Fechando Pedido");
+    let linkWpp = montarLInkWpp();
+    console.log(linkWpp);
+    window.open(linkWpp);
+
+}
+
+/* Monta o link e a mensagem para o redirecionamente até o Whatsapp*/
+function montarLInkWpp() {
+    let linkBase = "https://wa.me/5584991004360?text=";
+    
+    let precoTotal = precoSobremesaSelecionada + precoPratoSelecionado + precoBebidaSelecionada;
+    
+    let mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${pratoSelecionado}\n- Bebida: ${bebidaSelecionada}\n- Sobremesa: ${sobremesaSelecionada}\nTotal: R$ ${precoTotal.toFixed(2)}`;
+    
+    let mensagemCodificada = encodeURIComponent(mensagem);
+    return (linkBase + mensagemCodificada);
 }
